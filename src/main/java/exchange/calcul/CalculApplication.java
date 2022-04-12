20994281fdcf35112b28b4fc9fc39193803dab38
LeeSangYoeb <xyrho.v@gmail.com>
2022-04-13 00:17:12 +0900

    Init 프로젝트 신규생성
    - H2 database 생성
    Calcul DB 신규생성
    
    - Spring Boot 정상작동확인

---
+++ src/main/java/exchange/calcul/CalculApplication.java
@@ -0,0 +1,13 @@
+package exchange.calcul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exchange/calcul/CalculApplicationTests.java
@@ -0,0 +1,13 @@
+package exchange.calcul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
