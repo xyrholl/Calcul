d30962e5f76ecd7858b681d0486f9ad03663cd6c
LeeSangYoeb <xyrho.v@gmail.com>
2022-04-14 22:27:01 +0900

    Feat CurrencyApi 호출
    - requestCurrencyApi() 메서드 추가

---
+++ src/main/java/exchange/calcul/util/ApiConnect.java
@@ -0,0 +1,27 @@
+package exchange.calcul.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ApiConnect {
+
+    @Value("${evn.accesskey}")
+    private String key;
+    private String req_url = "http://apilayer.net/api/live?currencies=KRW,JPY,PHP,&source=USD&format=1";
+
+    public HttpEntity<String> requestCurrencyApi(){
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.ALL);
+        HttpEntity<?> requestMessage = new HttpEntity<>(headers);
+        req_url += "&access_key="+ key;
+        HttpEntity<String> response = restTemplate.postForEntity(req_url, requestMessage, String.class);
+        return response;
+    }
+    
+}
