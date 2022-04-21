9f56b9fd037fb9a2784c1a93a6100ac2af5ce500
LeeSangYoeb <xyrho.v@gmail.com>
2022-04-21 23:02:34 +0900

    Feat index.html 화면개발
    - CountryDto 생성
    화면에서 보여지는 데이터 표기
    
    - home() 컨트롤러 model 파라미터 추가
    셀렉트박스를 위한 데이터 추가

--- src/main/java/exchange/calcul/controller/viewController.java
+++ src/main/java/exchange/calcul/controller/viewController.java
@@ -1,13 +1,21 @@
 package exchange.calcul.controller;
 
+import exchange.calcul.dto.CountryDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class viewController {
 
-    @RequestMapping("/")
-    public String home(){
+    @GetMapping("/")
+    public String home(Model model){
+        List<CountryDto> list = new ArrayList<>();
+        model.addAttribute("countries", list);
         return "index";
     }
     
---
+++ src/main/java/exchange/calcul/dto/CountryDto.java
@@ -0,0 +1,12 @@
+package exchange.calcul.dto;
+
+import lombok.Data;
+
+@Data
+public class CountryDto {
+
+    private String countryName;
+    private String currencyName;
+    private String countryCurrencyName;
+
+}
